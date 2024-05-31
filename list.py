52f849ef8585c5ef72ed174c0991294708019670
samnejad92 <m.samnejad92@gmail.com>
2024-05-31 19:02:58 +0330

    v4.0.0

--- list.py
+++ list.py
@@ -26,8 +26,16 @@ myColors=["red","blue","green","grey","yellow","orange",3.6]
 #     else:
 #         print(f"{color} is not a color")
 #     index+=1
+# myCourses=["Python","Kotlin","Ionic"]
+# myCourses.append("JQuery")
+# myCourses.extend(["Jquery","Vue js"])
+# myCourses.insert(1,"jquery")
+# print(myCourses)
 myCourses=["Python","Kotlin","Ionic"]
-myCourses.append("JQuery")
-myCourses.extend(["Jquery","Vue js"])
-myCourses.insert(1,"jquery")
+last_item = myCourses.pop()
+first_item = myCourses.pop(0)
+print(f"last item is {last_item}")
+print(f"first item is {first_item}")
+myCourses.remove("Kotlin")
+# myCourses.clear()
 print(myCourses)
